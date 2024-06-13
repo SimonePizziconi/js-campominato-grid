1a56d6a290fed1f53651fc8614cc33bf97e72bfe
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-13 16:51:48 +0200

    fix: create grid on click

--- js/main.js
+++ js/main.js
@@ -5,28 +5,31 @@ const containerGrid = document.querySelector(".grid-container");
 // prendiamo il bottone da HTML
 const playButton = document.querySelector("button");
 
+
 // crea un evento al click del bottone
 playButton.addEventListener("click", 
     function(){
-        containerGrid.classList.add("play");
+        // elimina il bottone
         playButton.classList.add("none")
+    
+        // Ogni cella ha un numero progressivo, da 1 a 100.
+        for (let i = 1; i <= 100; i++){
+        let square = createElementWithClass("div", "box");
+        square.textContent = i;
+        containerGrid.append(square);
+
+            // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
+            square.addEventListener("click", 
+                function (){
+                this.classList.toggle("active");
+
+                // ed emetto un messaggio in console con il numero della cella cliccata.
+                console.log(`hai cliccato la cella con il numero ${i}`);
+                }
+            );
+        }
     }
 );
 
-// Ogni cella ha un numero progressivo, da 1 a 100.
-for (let i = 1; i <= 100; i++){
-    let square = createElementWithClass("div", "box");
-    square.textContent = i;
-    containerGrid.append(square);
 
-    // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
-    square.addEventListener("click", 
-        function (){
-            this.classList.toggle("active");
-
-            // ed emetto un messaggio in console con il numero della cella cliccata.
-            console.log(`hai cliccato la cella con il numero ${i}`);
-        }
-    )
-}
 
