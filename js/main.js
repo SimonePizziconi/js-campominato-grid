ac1bb4a362c7819801eee69ac5dcef9f77ae3be9
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-13 15:59:22 +0200

    feat: create addEventListener a playButton

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// prendiamo il container da HTML
+const containerGrid = document.querySelector(".grid-container");
 
+// prendiamo il bottone da HTML
+const playButton = document.querySelector("button");
+
+// crea un evento al click del bottone
+playButton.addEventListener("click", 
+    function(){
+        containerGrid.classList.add("play");
+        playButton.classList.add("none")
+    }
+);
 // Ogni cella ha un numero progressivo, da 1 a 100.
 
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
